810fb6ec2fba08eb1a702f3f8c44ff3ad3f911fb
Ivan Potyomkin <nexus.nm@yandex.ru>
2019-06-01 21:46:52 +0300

    Добавлен установщик

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Установка Curl
+#apt-get install curl
+# curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+# Установка Node Js (npm)
+# apt install nodejs
+
+# Установка Gulp и пакетов в проект
+npm install gulp
+npm install gulp
+npm install gulp-watch --save-dev
+npm install gulp-minify-css --save-dev
+npm install gulp-clean --save-dev
+npm install gulp-less --save-dev
+npm install path --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-autoprefixer --save-dev
