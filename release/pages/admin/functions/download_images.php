fbaa7b2fc73786e25a4c9d3fd10c6582b3526e8d
Ivan Potyomkin <nexus.nm@yandex.ru>
2019-06-02 02:44:56 +0300

    Апгрейд парсера изображения

--- release/pages/admin/functions/download_images.php
+++ release/pages/admin/functions/download_images.php
@@ -3,14 +3,28 @@ require $_SERVER['DOCUMENT_ROOT'].'/modules/require_libs.php';
 
 $query = R::getAll( 'SELECT `id`, `img` FROM posts WHERE img != ""' );
 
-PrintArray($query);
-
 foreach ($query as $post) {
   $id = $post['id'];
   $img_url = $post['img'];
 
   // Название изображения
   $img_filename = $id . '-' .  basename($img_url);
+
+  // Расширение изображения
+  $img_extension = pathinfo($img_url, PATHINFO_EXTENSION);
+
+  // Обрезаем лишние символы
+	$bad_symbols = array('.', '"', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '|', ':', '"', '<', '>', '?', '[', ']', ';', "'", ',', '/', '', '~', '`', '=');
+	$img_filename = str_replace($bad_symbols, '', $img_filename);
+
+  // Вырезаем расширение
+  $img_filename = str_replace($img_extension, '', $img_filename);
+
+  // Обрезаем слишком длинные названия
+  $img_filename = substr($img_filename, 0, 25);
+
+  // Подставляем расширение
+  $img_filename = $img_filename . '.' . $img_extension;
 
   // Путь на сервере к изображению
   $img_filepath = $_SERVER['DOCUMENT_ROOT'] . "//images//news//" . $img_filename;
